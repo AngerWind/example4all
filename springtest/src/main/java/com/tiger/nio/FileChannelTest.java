208924dab5024dd26629e2a5c24b3baf078ffc23
tiger.shen <tiger.shen@welab-inc.com>
2021-11-26 18:57:17 +0800

    11/26

--- springtest/src/main/java/com/tiger/nio/FileChannelTest.java
+++ springtest/src/main/java/com/tiger/nio/FileChannelTest.java
@@ -25,20 +25,36 @@ public class FileChannelTest {
         FileChannel channel = file.getChannel();
 
         ByteBuffer buffer = ByteBuffer.allocate(1024);
-        byte[] bytes = new byte[1024];
-
         int len;
         StringBuilder sb = new StringBuilder();
         while ((len = channel.read(buffer)) != -1) {
-            System.out.println("read " + len + "bytes");
+            // System.out.println("read " + len + "bytes");
             buffer.flip();
-            buffer.get(bytes, 0, len);
-            sb.append(new String(bytes, 0, len, StandardCharsets.UTF_8));
+            sb.append(new String(buffer.array(), 0, len, StandardCharsets.UTF_8));
             buffer.clear();
-
         }
         System.out.println(sb.toString());
         channel.close();
         file.close();
+    }
+
+    @Test
+    @SneakyThrows
+    public void transform(){
+
+        // 源channel传输到目的channel
+        try (RandomAccessFile fromFile = new RandomAccessFile("fromFile.txt", "rw");
+            FileChannel      fromChannel = fromFile.getChannel();
+
+            RandomAccessFile toFile = new RandomAccessFile("toFile.txt", "rw");
+            FileChannel toChannel = toFile.getChannel()) {
+
+            long position = 0;
+            long count = fromChannel.size();
+
+            fromChannel.transferTo(position, count, toChannel); // transformTo
+            toChannel.transferFrom(fromChannel, position, count); // transformFrom
+        }
+
     }
 }
