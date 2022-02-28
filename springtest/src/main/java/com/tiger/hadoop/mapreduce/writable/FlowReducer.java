c5df24955e71ebfee8b01ec844741c770568c1a0
anger_wind <s871403165@163.com>
2022-02-28 08:52:37 +0800

    2022 2 28

---
+++ springtest/src/main/java/com/tiger/hadoop/mapreduce/writable/FlowReducer.java
@@ -0,0 +1,28 @@
+package com.tiger.hadoop.mapreduce.writable;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @author Shen
+ * @version v1.0
+ * @Title FlowReduce
+ * @date 2022/2/26 17:39
+ * @description
+ */
+public class FlowReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context) throws IOException, InterruptedException {
+        long upFlow = 0;
+        long downFlow = 0;
+        long totalFlow = 0;
+        for (FlowBean each : values) {
+            upFlow += each.getUpFlow();
+            downFlow += each.getDownFlow();
+            totalFlow += each.getTotalFlow();
+        }
+        context.write(key, new FlowBean(upFlow, downFlow, totalFlow));
+    }
+}
